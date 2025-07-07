71c094422459113da8aa083848dafae32689554f
Kaliyappan1 <yappank17@gmail.com>
2025-07-07 18:06:22 +0530

    added hoursperday

--- main.tf
+++ main.tf
@@ -156,6 +156,7 @@ resource "aws_instance" "Splunk_uf" {
     Owner         = var.usermail
     UserEmail     = var.usermail
     RunQuotaHours = var.quotahours
+    HoursPerDay   = var.hoursperday
     Category      = var.category
     PlanStartDate = var.planstartdate
   }
